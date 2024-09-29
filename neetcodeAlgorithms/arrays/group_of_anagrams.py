1098478512c6348e9e527b31eef81739d5d179e4
juanB <juancruzberrios73@gmail.com>
2024-09-29 11:57:10 -0300

    NO HAY TIEMPO

---
+++ neetcodeAlgorithms/arrays/group_of_anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self,strs: list[str])->list[list[str]]:
+            my_hashes=[]
+            for word in range(len(strs)):
+                my_hash={}
+                for letter in range(len(strs[word])):
+                    my_hash[strs[word][letter]]=1+my_hash.get(strs[word][letter],0)
+                my_keys=list(my_hash.keys())
+                my_keys.sort()
+                sorted_hash=str({i: my_hash[i] for i in my_keys})
+                for i in my_hashes:
+                    if(sorted_hash in i):
+                        i.append(strs[word])
+                    else:
+                        my_hashes.append([strs[word]])
+                return my_hashes        
+strs = ["act","pots","tops","cat","stop","hat"]
+result=None
+result=Solution.groupAnagrams(result,strs)
+print(result)
